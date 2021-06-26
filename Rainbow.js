4d9b5b571a03cd2fab792e55f8519b04302d7a88
AniKyan <75213994+KnyazyanAni@users.noreply.github.com>
2021-06-26 13:17:47 +0400

    Add files via upload
    First commit

---
+++ Rainbow.js
@@ -0,0 +1,81 @@
+/*************Events on Box1*****************/
+box1.onmouseover= function colorChange(event)
+{
+event.target.style.backgroundColor="#FF0000";
+box1.onmouseover="event.stopPropagation()";
+  };
+box1.onmouseout=function colorReChange(event)
+{
+event.target.style.backgroundColor="#AA1100";
+box1.onmouseout="event.stopPropagation()";
+};
+/***************Events on Box2***********/
+box2.onmouseover= function colorChange(event)
+{
+event.target.style.backgroundColor="#FF7700 ";
+box2.onmouseover="event.stopPropagation()";
+  };
+  box2.onmouseout=function colorReChange(event)
+{
+event.target.style.backgroundColor="#FF4400";    
+box2.onmouseout="event.stopPropagation()";};
+/***************Events on Box3***********/
+box3.onmouseover= function colorChange(event)
+{
+event.target.style.backgroundColor="#FFD000 ";
+box3.onmouseover="event.stopPropagation()";
+  };
+  box3.onmouseout=function colorReChange(event)
+{
+event.target.style.backgroundColor="#FFBB11";    
+box3.onmouseout="event.stopPropagation()";};
+/***************Events on Box4***********/
+box4.onmouseover= function colorChange(event)
+{
+event.target.style.backgroundColor="#00FF00 ";
+box4.onmouseover="event.stopPropagation()";
+  };
+  box4.onmouseout=function colorReChange(event)
+{
+event.target.style.backgroundColor="#55AA00";    
+box4.onmouseout="event.stopPropagation()";};
+/***************Events on Box5***********/
+box5.onmouseover= function colorChange(event)
+{
+event.target.style.backgroundColor="#0000FF ";
+box5.onmouseover="event.stopPropagation()";
+  };
+  box5.onmouseout=function colorReChange(event)
+{
+event.target.style.backgroundColor="#000055";    
+box5.onmouseout="event.stopPropagation()";};
+/***************Events on Box6***********/
+box6.onmouseover= function colorChange(event)
+{
+event.target.style.backgroundColor="#8A2BE2";
+box6.onmouseover="event.stopPropagation()";
+  };
+  box6.onmouseout=function colorReChange(event)
+{
+event.target.style.backgroundColor="#310062";    
+box6.onmouseout="event.stopPropagation()";};
+/***************Events on Box7***********/
+box7.onmouseover= function colorChange(event)
+{
+event.target.style.backgroundColor="#C770F3";
+box7.onmouseover="event.stopPropagation()";
+  };
+  box7.onmouseout=function colorReChange(event)
+{
+event.target.style.backgroundColor="#4B0082";    
+box7.onmouseout="event.stopPropagation()";};
+/***************Events on Box8***********/
+box8.onmouseover= function colorChange(event)
+{
+event.target.style.backgroundColor="#F5FCFF ";
+box8.onmouseover="event.stopPropagation()";
+  };
+  box8.onmouseout=function colorReChange(event)
+{
+event.target.style.backgroundColor="#DBF3FA";    
+box8.onmouseout="event.stopPropagation()";};
